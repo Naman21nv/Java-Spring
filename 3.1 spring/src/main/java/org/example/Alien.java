533dd3cc598cb0d31a72bb05a190d0b9bd287d85
Naman21nv <namannv28@gmail.com>
2026-02-25 15:45:57 +0530

    Enhanced Alien class: Added showLanguage() method and improved console messages

--- 3.1 spring/src/main/java/org/example/Alien.java
+++ 3.1 spring/src/main/java/org/example/Alien.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Alien {
+
+    // Method to demonstrate alien coding behavior
     public void code(){
-        System.out.println(" coding");
+        System.out.println("Alien is coding in Java!");
+    }
+
+    // Added: New method to show alien's programming language
+    public void showLanguage(){
+        System.out.println("Alien prefers Spring Framework!");
     }
 }
