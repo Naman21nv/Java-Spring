18f7d984cec81a8fcd73063da0ff22f444bf77f5
Naman21nv <namannv28@gmail.com>
2026-02-25 20:05:27 +0530

    setter injection done in spring.xml for age property

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -3,6 +3,14 @@ package org.example;
 public class Alien {
 int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     // Method to demonstrate alien coding behavior
     public void code(){
 
